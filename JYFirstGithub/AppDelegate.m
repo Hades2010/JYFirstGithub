68b5ff0a2b373652229d5c19ca089e916d4a73fb
Hades2010 <tjufejyong2010@163.com>
2014-12-23 22:53:21 +0800

    测试修改项目代码

--- JYFirstGithub/AppDelegate.m
+++ JYFirstGithub/AppDelegate.m
@@ -17,6 +17,7 @@
 
 - (BOOL)application:(UIApplication *)application didFinishLaunchingWithOptions:(NSDictionary *)launchOptions {
     // Override point for customization after application launch.
+    //谨以此APP献给我爱的宗萌
     return YES;
 }
 
