2ea78bdf6c01166599f1633b2f3d4c0827e2c1e7
JinYong <tjufejyong2010@163.com>
2014-12-24 10:47:19 +0800

    添加pch文件

--- JYFirstGithub/AppDelegate.m
+++ JYFirstGithub/AppDelegate.m
@@ -17,7 +17,6 @@
 
 - (BOOL)application:(UIApplication *)application didFinishLaunchingWithOptions:(NSDictionary *)launchOptions {
     // Override point for customization after application launch.
-    //谨以此APP献给我爱的宗萌
     return YES;
 }
 
